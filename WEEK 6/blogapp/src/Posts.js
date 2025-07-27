5a05f3d8354deef15cc12c5843226cd012b89476
Leelareddy-15 <leelareddy981anandareddy@gmail.com>
2025-07-27 14:43:04 +0530

    4.ReactJS-HOL

---
+++ WEEK 6/blogapp/src/Posts.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Post from './Post';
+
+class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      hasError: false
+    };
+  }
+
+  loadPosts = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => this.setState({ posts: data }))
+      .catch(error => {
+        console.error("Error fetching posts:", error);
+        this.setState({ hasError: true });
+      });
+  };
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    alert("Something went wrong in the Posts component.");
+    console.log(error, info);
+  }
+
+  render() {
+    const { posts, hasError } = this.state;
+
+    if (hasError) {
+      return <h2>Something went wrong while loading posts.</h2>;
+    }
+
+    return (
+      <div>
+        <h1>Blog Posts</h1>
+        {posts.map(post => (
+          <Post key={post.id} title={post.title} body={post.body} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
